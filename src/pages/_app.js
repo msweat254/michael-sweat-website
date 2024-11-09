89bf67ca61ecb3dec39b745b648377e826f4dd15
Michael Sweat <michael.sweat@ionsolar.com>
2024-11-09 10:55:20 -0700

    Updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,14 @@
 import '../styles/globals.css';
 import Sidebar from '../components/Sidebar';
-import { TransitionProvider } from '../context/TransitionContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <TransitionProvider>
-      <div className="app-container">
-        <Sidebar />
-        <main className="main-content">
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </TransitionProvider>
+    <div className="app-container">
+      <Sidebar />
+      <main className="main-content">
+        <Component {...pageProps} />
+      </main>
+    </div>
   );
 }
 
