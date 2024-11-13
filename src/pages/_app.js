4ad9708691a4281d326040651a68cfaa37402a77
Michael Sweat <michael.sweat@ionsolar.com>
2024-11-12 21:51:44 -0700

    Updated head and icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import Sidebar from '../components/Sidebar';
+import Head from 'next/head';
+import { BsBorderAll } from 'react-icons/bs';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="app-container">
+      <Head>
+        <title>Michael Sweat</title>
+        <link rel='icon' href='/favicon.ico'></link>
+      </Head>
       <Sidebar />
       <main className="main-content">
         <Component {...pageProps} />
